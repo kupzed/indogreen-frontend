221b5ca51bdab590486d30e6af496fbb0732907d
kupzed <rizafahdansyahda3@gmail.com>
2025-08-20 14:15:51 +0700

    Update +page.svelte

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
     let formData = {
-      username: '',
-      about: '',
       name: '',
       email: '',
-      country: 'United States',
-      streetAddress: '',
-      city: '',
-      region: '',
-      postalCode: '',
+      country: 'User_1',
       project: true,
       activity: false,
       mitra: false,
@@ -119,10 +113,10 @@
                     autocomplete="country-name" 
                     class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                   >
-                    <option>United States</option>
-                    <option>Canada</option>
-                    <option>Mexico</option>
-                    <option>Indonesia</option>
+                    <option>User_1</option>
+                    <option>User_2</option>
+                    <option>User_3</option>
+                    <option>User_4</option>
                   </select>
                   <svg viewBox="0 0 16 16" fill="currentColor" data-slot="icon" aria-hidden="true" class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4">
                     <path d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" fill-rule="evenodd" />
