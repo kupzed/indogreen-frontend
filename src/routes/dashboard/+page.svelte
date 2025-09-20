f959a914eb090843c81f92cafee4585ad219da4b
kupzed <rizafahdansyahda3@gmail.com>
2025-09-20 15:26:54 +0700

    little fix darkmode in dashboard page

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -114,7 +114,7 @@
               <a href={`/activities/${activity.id}`} class="block hover:bg-gray-50 dark:hover:bg-neutral-950">
                 <div class="px-4 py-4 sm:px-6">
                   <div class="flex items-center justify-between">
-                    <p class="text-sm font-medium text-indigo-600 truncate">
+                    <p class="text-sm font-medium text-indigo-600 dark:text-indigo-400 truncate">
                       {activity.name}
                     </p>
                     <div class="ml-2 flex-shrink-0 flex">
