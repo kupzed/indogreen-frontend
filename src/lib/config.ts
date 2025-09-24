a26903b7a54614aea3210f9f0e6d8f3dd8e520aa
kupzed <rizafahdansyahda3@gmail.com>
2025-09-24 12:30:40 +0700

    Update dev server and API base URLs
    Changed the Vite dev server to use a specific host and port, and updated the API base URL to match the new backend address. This aligns frontend and backend configurations for local development.

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,2 @@
 // src/lib/config.ts
-export const API_BASE_URL = 'http://127.0.0.1:8000/api'; // Sesuaikan dengan URL backend Laravel Anda
+export const API_BASE_URL = 'http://192.168.1.44:8001/api'; // Sesuaikan dengan URL backend Laravel Anda
